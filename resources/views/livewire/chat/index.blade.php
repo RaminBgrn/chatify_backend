24f5073ff3e473c4982449b51630bd8a1f97aaee
RaminBgrn <ramin.bagheriian@gmail.com>
2024-11-07 00:04:21 +0330

    ui improve in main chat page

--- resources/views/livewire/chat/index.blade.php
+++ resources/views/livewire/chat/index.blade.php
@@ -1,11 +1,12 @@
 <x-app-layout>
     <div
-        class= "fixed h-full flex bg-gray-800 border-gray-700 lg:shadow-sm overflow-hidden inset-0 lg:top-16 lg:inset-x-2 m-auto lg:h-[90%] rounded-t-lg">
-        <div class="relative w-full md:w-[320px] xl:w-[400px] overflow-y-auto shrink-0 h-full border-gray-700 border">
+        class= "fixed h-full flex lg:shadow-sm overflow-hidden inset-0 lg:top-16 lg:inset-x-2 m-auto lg:h-[90%] rounded-t-lg">
+        <div
+            class="relative w-full md:w-[320px] xl:w-[400px] overflow-y-auto shrink-0 h-full rounded-lg mr-1  bg-gray-800">
             @include('livewire.chat.chat-list')
         </div>
 
-        <div class="hidden md:grid w-full border-gray-700 border-l h-full relative overflow-y-auto"
+        <div class="hidden md:grid w-full rounded-lg bg-gray-800 h-full relative overflow-y-auto ml-1"
             style="contain:content">
             <div class="m-auto text-center justify-center flex flex-col gap-3 ">
                 <h3 class="font-medium text-2xl text-gray-400 "> Choose convertion to start chating </h3>
