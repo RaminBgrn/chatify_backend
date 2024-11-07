177b1159fa2944fdb98657c71d1aa6d598424a20
RaminBgrn <ramin.bagheriian@gmail.com>
2024-11-07 19:42:57 +0330

    chat list header has been created and responsive

--- resources/views/livewire/chat/chat-box.blade.php
+++ resources/views/livewire/chat/chat-box.blade.php
@@ -1,3 +1,17 @@
 <div>
-    chatbox
+    <header class="w-full static inset-x-0 flex pb-[5px] pt-[5px] top-0 z-10 bg-gray-800 border-b border-gray-700">
+        <div class="flex w-full items-center px-2 lg:px-4 gap-2 md:gap-5">
+            <a class="shrink-0 lg:hidden" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                    class="bi bi-arrow-left-short fill-gray-200" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5" />
+                </svg>
+            </a>
+            <div class="shrink-0">
+                <x-avatar class="h-9 w-9 lg:w-11 lg:h-11" />
+            </div>
+            <h6 class="font-bold truncate text-gray-400">{{ fake()->name() }}</h6>
+        </div>
+    </header>
 </div>
